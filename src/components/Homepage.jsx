9d90ce0a19237d8f35e367c77f15736b6223c58a
tony-vargas <142276576+tony-vargas@users.noreply.github.com>
2024-04-15 20:25:06 -0700

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,7 +5,7 @@ const Homepage = ({user}) => {
         <div className="homePage">
             <h2>{user.firstname ? `Welcome back, ${user.firstname}!` : 'Welcome to our Library!'}</h2>
             <br />
-            <p>"If we could change ourselves, the tendencies in the world would aslo change. As a man changes his own nature, so does the attitude of the world change towards him." - Mahtama Gandhi</p>
+            <p>Welcome! Please check out our library selection from the links above.</p>
 
             <Link to='/books' className='homeToBooks'>To All Books</Link>
         </div>
